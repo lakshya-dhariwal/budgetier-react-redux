3d0e8f14551800af4dc774ab1f0caca080930726
lakshya-dhariwal <lakshyadhariwal9@gmail.com>
2022-01-22 18:34:02 +0530

    ➕ Tailwind

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       
-      </header>
+    <div className="App bg-nav-blue min-h-full min-w-full">
+      <header className="App-header">Hello</header>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "nav-blue": "#2A5578",
+        "custom-red": "#EE825F",
+        "custom-purple": "#6765ED",
+      },
+    },
+  },
+  plugins: [],
+};
